5421d530faf497cd85d48b5178541ac35841f4d9
thanyiz <2825406930@qq.com>
2024-09-24 10:57:16 +0800

    hadoop

---
+++ hadoop/hadoop_etc/hadoop-env.sh
@@ -0,0 +1,9 @@
+export JAVA_HOME=/opt/java/openjdk
+export HADOOP_HOME=/opt/hadoop
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export HADOOP_LOG_DIR=$HADOOP_HOME/logs
+export HDFS_NAMENODE_USER=root
+export HDFS_DATANODE_USER=root
+export HDFS_SECONDARYNAMENODE_USER=root
+export YARN_RESOURCEMANAGER_USER=root
+export YARN_NODEMANAGER_USER=root
