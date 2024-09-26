a078aa7e376b857b09432ffa37df042c610b2780
thanyiz <2825406930@qq.com>
2024-09-27 01:44:23 +0800

    mysql

---
+++ hive/hadoop_etc/yarn-env.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME=/opt/java/openjdk
+export HADOOP_HOME=/opt/hadoop
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export HADOOP_LOG_DIR=$HADOOP_HOME/logs
+export YARN_PROXYSERVER_USER=root
